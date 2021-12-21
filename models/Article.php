5af1fd6c884e14b5ac98d84d1a5d6f7dfcc97600
akymenkovlad <akymenkovlad16@gmail.com>
2021-12-21 15:38:06 +0200

    Added Pagination and article display

--- models/Article.php
+++ models/Article.php
@@ -122,4 +122,9 @@ class Article extends \yii\db\ActiveRecord
         }
         return '/no-image.png';
     }
+    public function getDate(){
+
+        return Yii::$app->formatter->asDate($this->date);
+
+    }
 }
--- views/site/index.php
+++ views/site/index.php
@@ -1,70 +1,38 @@
 <div class="col-md-8">
+    <?php use yii\helpers\Url;
 
-    <article class="post">
-
-        <div class="post-thumb">
-
-            <a href=""><img class="img-index" src="" alt="Image"></a>
-
-        </div>
-
-        <div class="post-content">
-
-            <header class="entry-header text-center text-uppercase">
-
-                <h6><a href=""> Sport</a></h6>
-
-                <h1 class="entry-title"><a href=""> Home is peaceful place </a></h1>
-
-            </header>
-
-            <div class="entry-content">
-
-                <p>
-
-                    Text
-
-                </p>
-
-                <div class="btn-continue-reading text-center text-uppercase">
-
-                    <a href="#" class="more-link">Continue Reading</a>
-
+    foreach ($articles as $article): ?>
+        <article class="post">
+            <div class="post-thumb">
+                <a href=""><img class="img-index" src="<?= $article->getImage() ?> " alt="Image"></a>
+            </div>
+            <div class="post-content">
+                <header class="entry-header text-center text-uppercase">
+                    <h6><a href="<?= Url::toRoute(['/topic', 'id' => $article->topic->id]) ?>"> <?= $article->topic->name; ?></a></h6>
+                    <h1 class="entry-title"><a href="<?= Url::toRoute(['/view', 'id'=>$article->id]) ?>"> <?= $article->title; ?> </a></h1>
+                </header>
+                <div class="entry-content">
+                    <p> <?= mb_strimwidth($article->description,0, 360, "...");  ?> </p>
+                    <div class="btn-continue-reading text-center text-uppercase">
+                        <a href="<?= Url::toRoute(['/view', 'id'=>$article->id]) ?>" class="more-link">Continue Reading</a>
+                    </div>
                 </div>
-
+                <div class="social-share">
+                    <span class="social-share-title pull-left text-capitalize">By <?= $article->user->name;?> On <?= $article->getDate();?></span>
+                    <ul class="text-center pull-right">
+                        <li><a class="s-facebook" href="#"><i class="fa fa-eye"></i></a></li>
+                        <?= (int)$article->viewed; ?>
+                    </ul>
+                </div>
             </div>
-
-            <div class="social-share">
-
-                <span class="social-share-title pull-left text-capitalize">By Vlad On 21-12-2021</span>
-
-                <ul class="text-center pull-right">
-
-                    <li><a class="s-facebook" href="#"><i class="fa fa-eye"></i></a></li>
-
-                    10
-
-                </ul>
-
-            </div>
-
-        </div>
-
-    </article>
-
-    <ul class="pagination">
-
-        <li class="active"><a href="#">1</a></li>
-
-        <li><a href="#">2</a></li>
-
-        <li><a href="#">3</a></li>
-
-    </ul>
-
+        </article>
+    <?php endforeach;
+    echo \yii\widgets\LinkPager::widget([
+        "pagination" => $pagination,
+    ])
+    ?>
 </div>
+
 <?php
-
-echo \Yii::$app->view->renderFile('@app/views/site/right.php');
-
+echo \Yii::$app->view->renderFile('@app/views/site/right.php',  compact('popular','recent','topics'));
 ?>
