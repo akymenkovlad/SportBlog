ba3b366d8c8174d10a85e070a3e7adc9b3afc43a
Bohdan Malynovskyi <mbu.bogdan@gmail.com>
2021-12-23 00:48:40 +0200

    Comments

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -12,6 +12,8 @@ use app\models\ContactForm;
 use app\models\Topic;
 use app\models\Article;
 use yii\data\Pagination;
+use app\models\Comment;
+use app\models\CommentForm;
 
 class SiteController extends Controller
 {
@@ -146,6 +148,22 @@ class SiteController extends Controller
 
         $topics = Topic::find()->all();
 
+        $comments = $article->comments;
+
+        $commentsParent = array_filter($comments, function ($k) {
+
+            return $k['comment_id'] == null;
+
+        });
+
+        $commentsChild = array_filter($comments, function ($k) {
+
+            return ($k['comment_id'] != null && !$k['delete']);
+
+        });
+
+        $commentForm = new CommentForm();
+
 
         return $this->render('single', [
 
@@ -156,6 +174,12 @@ class SiteController extends Controller
             'recent' => $recent,
 
             'topics' => $topics,
+
+            'commentsParent' => $commentsParent,
+
+            'commentsChild' => $commentsChild,
+
+            'commentForm' => $commentForm,
         ]);
     }
 
@@ -201,6 +225,46 @@ class SiteController extends Controller
             'topics' => $topics,
 
         ]);
+
+    }
+
+    public function actionComment($id, $id_comment = null)
+    {
+
+        $model = new CommentForm();
+
+        if (Yii::$app->request->isPost) {
+
+            $model->load(Yii::$app->request->post());
+
+            if ($model->saveComment($id, $id_comment)) {
+
+                return $this->redirect(['site/view', 'id' => $id]);
+
+            }
+
+        }
+
+    }
+
+    public function actionCommentDelete($id, $id_comment)
+    {
+
+        if (Yii::$app->request->isPost) {
+
+            $data = Comment::findOne($id_comment);
+
+            if ($data->user_id == Yii::$app->user->id) {
+
+                $data->delete = true;
+
+                $data->save(false);
+
+            }
+
+            return $this->redirect(['site/view', 'id' => $id]);
+
+        }
 
     }
 }
